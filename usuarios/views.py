b5dd318cbf01c0e37158f9870a8bab5574490649
waltercjunior <waltercjunior@gmail.com>
2023-01-29 23:01:55 -0300

    Commit inclusão de templates e static

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Olá mundo!")
+
+    context = {
+        "nome_pagina": "Dashboard",
+    }
+
+    return render(request, "index.html", context)
